3bd6a5bbcec2b1180a8ab3df2807fffd2a07563d
Magenon <nocpolaco@gmail.com>
2023-09-08 16:01:52 -0300

    Update

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,7 @@
 //import React from 'react'
 import { Link } from "react-router-dom";
 import './Header.css'
-import Navbar from "../../components/Navbar";
+import Navbar from "../Navbar";
 
 function Header() {
     return (
