d016e1dd9ceccc4fb937d6227302f35ad2aa3875
Magenon <nocpolaco@gmail.com>
2023-09-09 22:25:31 -0300

    Updates

--- src/components/Subtitle/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import classes from './index.module.css';
-
-export default function Subtitle(props) {
-  // eslint-disable-next-line react/prop-types
-  return <h2 className={classes.Container}>{props.children}</h2>;
-}
--- src/components/Title/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-import classes from './index.module.css';
-
-export default function Title(props) {
-  // eslint-disable-next-line react/prop-types
-  return <h1 className={classes.Container}>{props.children}</h1>;
-}
